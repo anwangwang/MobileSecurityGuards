a5788b5f3cf498195337870228c5aa3349ea3618
anwangwang <awwhome@163.com>
2017-03-27 10:42:10 +0800

    1.创建splash界面 2.文本框阴影效果

---
+++ app/src/main/java/com/awwhome/mobilesecurityguards/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.awwhome.mobilesecurityguards.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.awwhome.mobilesecurityguards.R;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
