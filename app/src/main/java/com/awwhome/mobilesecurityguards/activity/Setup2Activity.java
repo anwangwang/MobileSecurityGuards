8b4592d5b8fb5311dd9c2a555af4ff20fba5d823
anwangwang <awwhome@163.com>
2017-04-12 07:51:23 +0800

    1.第二个设置页面 2.上一页，下一页按钮的属性抽取

--- app/src/main/java/com/awwhome/mobilesecurityguards/activity/Setup2Activity.java
+++ app/src/main/java/com/awwhome/mobilesecurityguards/activity/Setup2Activity.java
@@ -1,33 +1,40 @@
 package com.awwhome.mobilesecurityguards.activity;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.awwhome.mobilesecurityguards.R;
 
 /**
- * 手机防盗第一个设置Activity
- * Created by awwho on 2017/4/8.
+ * 第二个设置Activity
+ * Created by awwho on 2017/4/12.
  */
-public class Setup1Activity extends Activity {
+public class Setup2Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setup1);
+
+        setContentView(R.layout.activity_setup2);
+    }
+
+    /**
+     * 上一页
+     *
+     * @param view
+     */
+    public void prePage(View view) {
+
     }
 
     /**
      * 下一页
-     * 跳转到第二个设置页面
      *
      * @param view
      */
     public void nextPage(View view) {
-        Intent intent = new Intent(getApplicationContext(), Setup2Activity.class);
-        startActivity(intent);
-        finish();
+
     }
+
 }
