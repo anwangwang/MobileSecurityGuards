180a53da333cb55ff2800cbe9b3ad92771fb5a4e
anwangwang <awwhome@163.com>
2017-04-19 07:20:54 +0800

    手势滑动效果

--- app/src/main/java/com/awwhome/mobilesecurityguards/activity/Setup1Activity.java
+++ app/src/main/java/com/awwhome/mobilesecurityguards/activity/Setup1Activity.java
@@ -3,6 +3,8 @@ package com.awwhome.mobilesecurityguards.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.awwhome.mobilesecurityguards.R;
@@ -13,10 +15,43 @@ import com.awwhome.mobilesecurityguards.R;
  */
 public class Setup1Activity extends Activity {
 
+    private static final String TAG = "Setup1Activity";
+
+    private GestureDetector gestureDetector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup1);
+
+        gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+
+            /**
+             * 监听手势的移动
+             * @param e1 起始点
+             * @param e2 结束点
+             * @param velocityX 水平方向移动的速度
+             * @param velocityY 垂直方向移动的速度
+             * @return
+             */
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                if (e1.getX() - e2.getX() > 0) {
+                    // 由右向左滑动，滑动到下一页
+                    Intent intent = new Intent(getApplicationContext(), Setup2Activity.class);
+                    startActivity(intent);
+                    finish();
+
+                    // 开启平移动画
+                    overridePendingTransition(R.anim.next_in_anim, R.anim.next_out_anim);
+                }
+                if (e1.getX() - e2.getX() < 0) {
+                    // 由左向右滑动，滑动到上一页
+
+                }
+                return super.onFling(e1, e2, velocityX, velocityY);
+            }
+        });
     }
 
     /**
@@ -32,5 +67,13 @@ public class Setup1Activity extends Activity {
 
         // 开启平移动画
         overridePendingTransition(R.anim.next_in_anim, R.anim.next_out_anim);
+    }
+
+    // 监听屏幕上响应的时间类型
+    // 按下(一次) 移动(多次) 抬起(一次)
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 }
