7cab2ffd55f3d768e27906d50e2dd9bf795a009b
anwangwang <awwhome@163.com>
2017-04-19 19:56:40 +0800

    查询归属地布局

--- app/src/main/java/com/awwhome/mobilesecurityguards/activity/QueryPhoneAddressActivity.java
+++ app/src/main/java/com/awwhome/mobilesecurityguards/activity/QueryPhoneAddressActivity.java
@@ -2,6 +2,10 @@ package com.awwhome.mobilesecurityguards.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.awwhome.mobilesecurityguards.R;
 
@@ -12,11 +16,40 @@ import com.awwhome.mobilesecurityguards.R;
 public class QueryPhoneAddressActivity extends Activity {
 
     private static final String TAG = "QueryPhoneAddressActivi";
+    private EditText et_phone;
+    private Button btn_query;
+    private TextView tv_query_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_query_phone_address);
+
+        initView();
+        initData();
+    }
+
+    /**
+     * 初始化数据
+     */
+    private void initData() {
+    }
+
+    /**
+     * 初始化UI
+     */
+    private void initView() {
+
+        et_phone = (EditText) findViewById(R.id.et_phone);
+        btn_query = (Button) findViewById(R.id.btn_query);
+        tv_query_result = (TextView) findViewById(R.id.tv_query_result);
+
+        btn_query.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
