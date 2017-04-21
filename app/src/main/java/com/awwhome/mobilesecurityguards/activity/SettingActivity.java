516f8e2cb680993a46a98cd9c5e188cb8ef060a0
anwangwang <awwhome@163.com>
2017-04-21 09:37:34 +0800

    在服务中监听电话的状态

--- app/src/main/java/com/awwhome/mobilesecurityguards/activity/SettingActivity.java
+++ app/src/main/java/com/awwhome/mobilesecurityguards/activity/SettingActivity.java
@@ -1,10 +1,12 @@
 package com.awwhome.mobilesecurityguards.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.awwhome.mobilesecurityguards.R;
+import com.awwhome.mobilesecurityguards.service.AddressService;
 import com.awwhome.mobilesecurityguards.utils.ConstantValue;
 import com.awwhome.mobilesecurityguards.utils.SpUtil;
 import com.awwhome.mobilesecurityguards.widget.SettingItemView;
@@ -16,18 +18,80 @@ import com.awwhome.mobilesecurityguards.widget.SettingItemView;
 public class SettingActivity extends Activity {
 
     private SettingItemView siv_update;
+    private SettingItemView siv_address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
-        initUpdateView();
-        initData();
+//        initUpdateView();
+        initUpdate();
+//        initAddressView();
+        initAddress();
+//        initData();
     }
 
     /**
-     * 初始化事件
+     * 初始化归属地开启
+     */
+    private void initAddress() {
+        siv_address = (SettingItemView) findViewById(R.id.siv_address);
+
+        // 注册点击事件
+        siv_address.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean checked = siv_address.isChecked();
+                siv_address.setCheck(!checked);
+                if (!checked) {
+                    // 开启服务
+                    startService(new Intent(getApplicationContext(), AddressService.class));
+                } else {
+                    // 关闭服务
+                    stopService(new Intent(getApplicationContext(), AddressService.class));
+                }
+            }
+        });
+
+
+    }
+
+    /**
+     * 初始化自动更新
+     */
+    private void initUpdate() {
+        siv_update = (SettingItemView) findViewById(R.id.siv_update);
+        // 获取上一次存储的节点值
+        boolean open_update = SpUtil.getBoolean(this, ConstantValue.OPEN_UPDATE, false);
+        // 将节点值设置给siv_update控件
+        siv_update.setCheck(open_update);
+
+        // 自动更新点击事件
+        siv_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 判断当前条目是否选中
+                boolean checked = siv_update.isChecked();
+                // 若选中，修改为为选中
+                // 若未选中，修改为选中
+                siv_update.setCheck(!checked);
+
+                // 当控件点击后的状态发生改变时，存储新的节点值
+                SpUtil.putBoolean(getApplicationContext(), ConstantValue.OPEN_UPDATE, !checked);
+            }
+        });
+    }
+
+    /**
+     * 初始化归属地开启UI
+     */
+    private void initAddressView() {
+        siv_address = (SettingItemView) findViewById(R.id.siv_address);
+    }
+
+    /**
+     * 初始化数据
      */
     private void initData() {
 
---
+++ app/src/main/java/com/awwhome/mobilesecurityguards/service/AddressService.java
@@ -0,0 +1,67 @@
+package com.awwhome.mobilesecurityguards.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+/**
+ * 归属地悬浮框显示服务
+ * Created by awwho on 2017/4/21.
+ */
+public class AddressService extends Service {
+
+    private static final String TAG = "AddressService";
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // 当服务第一次开启时，显示土司
+        // 监听电话的状态，当电话处于响铃状态时，显示土司，处于空闲状态时，不显示土司
+        // 1.获取电话管理者对象
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        // 2.监听电话状态
+        telephonyManager.listen(new PhoneStateListener() {
+            @Override
+            public void onCallStateChanged(int state, String incomingNumber) {
+                super.onCallStateChanged(state, incomingNumber);
+                // 3.电话状态发生改变时，调用此方法
+                switch (state) {
+                    case TelephonyManager.CALL_STATE_IDLE:
+                        // 空闲状态，不显示土司
+                        Log.d(TAG, "onCallStateChanged: 空闲状态，土司不显示了。。。。");
+                        break;
+                    case TelephonyManager.CALL_STATE_OFFHOOK:
+                        // 摘机状态
+                        break;
+                    case TelephonyManager.CALL_STATE_RINGING:
+                        // 响铃状态，显示土司
+                        Log.d(TAG, "onCallStateChanged: 响铃状态，土司显示了。。。。。。");
+                        break;
+                }
+
+            }
+        }, PhoneStateListener.LISTEN_CALL_STATE);
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // 当服务关闭时，不显示土司
+    }
+}
